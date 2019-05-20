d5155ac709d98b196ecabff578b67b9b9c869741
Jun Ito <jun_ito@voyagegroup.com>
2019-05-20 15:21:19 +0900

    Add: target movies

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ import tmdbsimple as tmdb
 
 
 def main():
-    print(os.environ.get("API_KEY"))
+    target_movie_ids = [
+        447404,  # Detective Pikachu
+        299534,  # Avengers Endgame
+        118,  # Charlie and the Chocolate Factory
+        109445,  # Frozen
+        335984,  # Blade Runner 2049 (because we already have a poster, it is easy to show demo for visitor)
+        315011,  # Shin Godzilla
+    ]
+
+    tmdb.API_KEY = os.environ.get("API_KEY")
+    for movie_id in target_movie_ids:
+        movie = tmdb.Movies(movie_id)
+        response = movie.info()
+        print(movie.title)
 
 
 if __name__ == "__main__":
